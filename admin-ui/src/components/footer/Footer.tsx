cdd923b3a4a468337e5343c80e3365163aae3478
huykaiser <huy@rawskill.gg>
2023-11-08 17:16:49 +0100

    upload server and client

---
+++ admin-ui/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>huykaiser</span>
+      <span>&copy; Huy Kaiser Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
